1cf72543b96c280a2b6a152533f48081d9089834
unknown <bhavaikya21@gmail.com>
2020-09-11 00:05:34 -0700

    initial commit

---
+++ myapp/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from myapp.models import *
+# Create your views here.
+def create_topic(request):
+    if request.method=="POST":
+        topic=request.POST.get("topic")
+        t=Topic.objects.get_or_create(topic_name=topic)
+        if t[1]==True:
+            t[0].save()
+            return HttpResponse("<h3>Topic Added Successfully</h3>")
+        else:
+            return HttpResponse("<h3>Topic Is Already Exist In Table</h3>")
+    return render(request,"create_topic.html")
+
+def create_webpage(request):
+    if request.method=="POST":
+        topic=request.POST.get('topic')
+        name=request.POST.get('name')
+        url=request.POST.get("url")
+        t=Topic.objects.get_or_create(topic_name=topic)[0]
+        w=Webpage.objects.get_or_create(topic=t,name=name,url=url)[0]
+        w.save()
+        return HttpResponse("<h3>Webpage Added Successfully</h3>")
+    topics=Topic.objects.all()
+    return render(request,"create_webpage.html",context={'topics':topics})
+
+def display_topics(request):
+    topics=Topic.objects.all()
+    return render(request,"display_topic.html",\
+        context={'topics':topics})
+
+def display_webpages(request):
+    webpages=Webpage.objects.all()
+    return render(request,"display_webpage.html",\
+        context={'webpages':webpages})
+#accessing the specific data present in the database by passing the pk
+#in the url
+def display_topic(request,id):
+    topics=Topic.objects.filter(id=id)
+    return render(request,"display_topic.html",\
+        context={'topics':topics})
+
+def display_webpage(request,webid):
+    webpages=Webpage.objects.filter(id=webid)
+    return render(request,"display_webpage.html",\
+        context={'webpages':webpages})
+
+def search_webpage(request):
+    if request.GET.get('search'):
+        id=request.GET['search']
+        return redirect('display_webpage',webid=id)
+    return render(request,'search_webpage.html')
+
